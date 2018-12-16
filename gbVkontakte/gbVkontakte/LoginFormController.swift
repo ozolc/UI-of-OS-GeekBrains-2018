facde668147592367aef44f4b6f9d266d958de84
Maksim Nosov <ozolc@yandex.ru>
2018-12-16 18:42:31 +0300

    First gbVkontakte commit

---
+++ gbVkontakte/gbVkontakte/LoginFormController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginFormController.swift
+//  gbVkontakte
+//
+//  Created by Maksim Nosov on 16/12/2018.
+//  Copyright © 2018 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
