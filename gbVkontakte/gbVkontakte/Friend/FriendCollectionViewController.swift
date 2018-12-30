273d917d888b4d30a3da775878f4d8820e9206c0
Maksim Nosov <ozolc@yandex.ru>
2018-12-30 17:20:52 +0300

    Added more quality images

--- gbVkontakte/gbVkontakte/Friend/FriendCollectionViewController.swift
+++ gbVkontakte/gbVkontakte/Friend/FriendCollectionViewController.swift
@@ -37,18 +37,18 @@ class FriendCollectionViewController: UICollectionViewController {
         cell.friendImageView.image = self.friendImage
         cell.friendNameLabel.text = self.friendName
         
-        cell.contentView.layer.cornerRadius = 10
-        cell.contentView.layer.borderWidth = 1.0
-
-        cell.contentView.layer.borderColor = UIColor.clear.cgColor
-        cell.contentView.layer.masksToBounds = true
-
-        cell.layer.shadowColor = UIColor.gray.cgColor
-        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-        cell.layer.shadowRadius = 2.0
-        cell.layer.shadowOpacity = 1.0
-        cell.layer.masksToBounds = false
-        cell.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:cell.contentView.layer.cornerRadius).cgPath
+//        cell.contentView.layer.cornerRadius = 10
+//        cell.contentView.layer.borderWidth = 1.0
+//
+//        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+//        cell.contentView.layer.masksToBounds = true
+//
+//        cell.layer.shadowColor = UIColor.gray.cgColor
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+//        cell.layer.shadowRadius = 2.0
+//        cell.layer.shadowOpacity = 1.0
+//        cell.layer.masksToBounds = false
+//        cell.layer.shadowPath = UIBezierPath(roundedRect:cell.bounds, cornerRadius:cell.contentView.layer.cornerRadius).cgPath
     
         return cell
     }
