16c222aa567486ff21d555c55fbf32546ab87952
Maksim Nosov <ozolc@yandex.ru>
2019-01-22 12:57:54 +0300

    Добавил GestureRecognizer на изображения

--- gbVkontakte/gbVkontakte/News/NewsTableViewCell.swift
+++ gbVkontakte/gbVkontakte/News/NewsTableViewCell.swift
@@ -13,10 +13,52 @@ class NewsTableViewCell: UITableViewCell {
     @IBOutlet weak var newsTextLabel: UILabel!
     @IBOutlet weak var newsImageView: UIImageView!
     
+    @IBOutlet weak var likeImageView: UIImageView!
+    @IBOutlet weak var shareImageView: UIImageView!
+    @IBOutlet weak var commentImageView: UIImageView!
+    
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var shareLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    var likes = 0
+    var shares = 0
+    var comments = 0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+     
+        setupGestureRecognizer(likeImageView)
+        setupGestureRecognizer(shareImageView)
+        setupGestureRecognizer(commentImageView)
     }
+    
+    func setupGestureRecognizer(_ localSender: UIImageView) {
+        let gestureTap = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(sender:)))
+        localSender.isUserInteractionEnabled = true
+        gestureTap.numberOfTapsRequired = 1
+        localSender.addGestureRecognizer(gestureTap)
+    }
+    
+    @objc func imageViewTapped(sender: UIGestureRecognizer) {
+        switch sender.view {
+        case likeImageView:
+            self.likes += 1
+            likeLabel.text = String(likes)
+        case shareImageView:
+            self.shares += 1
+            shareLabel.text = String(shares)
+        case commentImageView:
+            self.comments += 1
+            commentLabel.text = String(comments)
+        default:
+            return
+        }
+    }
+    
+    
+
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
