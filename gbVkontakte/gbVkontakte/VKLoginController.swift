bccc4ac61d68cda6a0f58a0a4570a1dd5d61da36
Maksim Nosov <ozolc@yandex.ru>
2019-02-09 17:06:48 +0300

    Добавлена авторизация в ВК+получение списка групп

---
+++ gbVkontakte/gbVkontakte/VKLoginController.swift
@@ -0,0 +1,103 @@
+//
+//  VKLoginController.swift
+//  gbVkontakte
+//
+//  Created by Maksim Nosov on 09/02/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Alamofire
+
+class VKLoginController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authorizeOnVK()
+    }
+    
+    private func authorizeOnVK() {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "oauth.vk.com"
+        urlComponents.path = "/authorize"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "6853611"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "scope", value: "262150"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.85")
+        ]
+        
+        let request = URLRequest(url: urlComponents.url!)
+        
+        webView.load(request)
+    }
+    
+}
+
+extension VKLoginController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment  else {
+                decisionHandler(.allow)
+                return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        print(params)
+        
+        guard let token = params["access_token"], let userId = Int(params["user_id"]!) else {
+            decisionHandler(.cancel)
+            return
+        }
+        
+        Session.shared.token = token
+        Session.shared.userId = userId
+        
+        print(token, userId)
+        loadGroups()
+//        performSegue(withIdentifier: "VKLogin", sender: nil)
+        decisionHandler(.cancel)
+    }
+    
+    func loadGroups() {
+        
+        let baseUrl = "https://api.vk.com"
+        let path = "/method/groups.get"
+        
+        let params: Parameters = [
+            "access_token" : Session.shared.token,
+            "extended" : 1,
+            "v": "5.85"
+        ]
+        
+        Alamofire.request(baseUrl+path, method: .get, parameters: params).responseJSON { response in
+            guard let value = response.value else { return }
+            
+            print(value)
+        }
+    }
+    
+}
