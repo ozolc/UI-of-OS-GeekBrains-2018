a5d35450d7f4e097e3f410fb5b89bfefaa0c0694
Maksim Nosov <ozolc@yandex.ru>
2019-03-05 09:40:36 +0300

    Added RealmProvider

---
+++ gbVkontakte/gbVkontakte/Services/RealmProvider.swift
@@ -0,0 +1,42 @@
+//
+//  RealmProvider.swift
+//  gbVkontakte
+//
+//  Created by Maksim Nosov on 05/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import RealmSwift
+
+class RealmProvider {
+    static let deleteIfMigration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+    
+    static func save<T: Object>(items: [T],
+                                config: Realm.Configuration = Realm.Configuration(deleteRealmIfMigrationNeeded: true),
+                                update: Bool = true) {
+        
+        do {
+            let realm = try Realm(configuration: self.deleteIfMigration)
+            
+            try realm.write {
+                realm.add(items, update: true)
+            }
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    static func get<T: Object>(_ type: T.Type,
+                               config: Realm.Configuration = Realm.Configuration.defaultConfiguration) -> Results<T>? {
+        do {
+            let realm = try Realm(configuration: self.deleteIfMigration)
+            return realm.objects(type)
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
+    
+}
